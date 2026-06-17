a52f65516227e68782fca34c94fc9c1c6d40e9c5
R-S-P-777 <shubhamp232104@gmail.com>
2026-06-17 17:55:15 +0000

    day2

---
+++ project1.c
@@ -0,0 +1,23 @@
+// my first problum
+// area of rectangle
+#include <stdio.h>
+int main(){
+     //int length;
+     //int width;
+     // uper ke is do line ko aise ek hi line me  direct likh sakte hai
+     int length, width;
+     
+     printf("enter length =\n");
+     scanf("%d", &length);
+     
+     printf("enter width =\n");  
+     scanf("%d", &width);
+     
+     printf("the area of a rectangle is %d", length*width);
+    return 0;
+}    
+
+
+
+
+
