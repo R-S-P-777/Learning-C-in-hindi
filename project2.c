876c7e80e090a32646bb0e379f74e967bc707722
R-S-P-777 <shubhamp232104@gmail.com>
2026-06-17 18:01:07 +0000

    day2

---
+++ project2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int r;
+    int h;
+    //area of circle
+    printf("enter the value of radius:\n");
+    scanf("%d", &r);
+    printf("the area of circle is %f\n\n", 22.0/7*r*r);
+    
+    //volume of cylender
+    printf("enter the valye of hihkt of cylender :");
+    scanf("%d", &h);
+    
+    printf("volume of cylender is : %f", 22.0/7*r*r*h);
+        
+    return 0;
+    
+}    
